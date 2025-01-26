1544acfcf65faad6de00541a3c85337ea286cb98
tanmayrana222 <tanmayraa@gmail.com>
2025-01-26 23:35:41 +0530

    changes

--- experiment1/SwapStrings.java
+++ experiment1/SwapStrings.java
@@ -2,22 +2,20 @@ import java.util.Scanner;
 
 public class SwapStrings {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Enter the first string: ");
-        String string1 = scanner.nextLine();
-
-        System.out.print("Enter the second string: ");
-        String string2 = scanner.nextLine();
-
-        String temp = string1;
-        string1 = string2;
-        string2 = temp;
-
-        System.out.println("After swapping:");
-        System.out.println("First string: " + string1);
-        System.out.println("Second string: " + string2);
-
-        scanner.close();
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Enter the first string: ");
+            String string1 = scanner.nextLine();
+            
+            System.out.print("Enter the second string: ");
+            String string2 = scanner.nextLine();
+            
+            String temp = string1;
+            string1 = string2;
+            string2 = temp;
+            
+            System.out.println("After swapping:");
+            System.out.println("First string: " + string1);
+            System.out.println("Second string: " + string2);
+        }
     }
 }
